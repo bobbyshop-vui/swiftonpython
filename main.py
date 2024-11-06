514c6dab6035f537ee67dfca21d4693e2fed94dc
hqmdokkai <155129956+hqmdokkai@users.noreply.github.com>
2024-11-06 20:37:55 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+import swiftonpython  # Nhập toàn bộ gói swiftonpython
+
+# Mã Swift cần thực thi
+swift_code = """
+print("Hello from Swift!")
+"""  # Giữ nguyên định dạng chuỗi
+
+# Gọi hàm để thực thi mã Swift
+swiftonpython.swift(swift_code)  # Truyền chuỗi
